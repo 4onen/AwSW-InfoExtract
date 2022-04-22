0102d4dfea1ef4a2e3e31bd11f12c343294584fb
Matthew Dupree <mattjdupree@gmail.com>
2022-04-22 13:22:32 -0700

    Empty the __init__.py file.

--- __init__.py
+++ __init__.py
@@ -1,14 +1,1 @@
-from modloader.modclass import Mod, loadable_mod
-@loadable_mod
-class AWSWMod(Mod):
-    name = "Information Extractor"
-    version = "DO NOT USE . DO NOT DISTRIBUTE"
-    author = "4onen"
-    
-    @staticmethod
-    def mod_load():
-        pass
-
-    @staticmethod
-    def mod_complete():
-        pass
+# This module intentionally left blank.
